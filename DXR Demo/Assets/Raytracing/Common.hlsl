524eac16da84dd2c74feae8046dbaadbc803f706
LowerDeckBoy <pa.krawiec@onet.pl>
2023-07-03 17:39:05 +0200

    Updated Raytracing shaders

--- DXR Demo/Assets/Raytracing/Common.hlsl
+++ DXR Demo/Assets/Raytracing/Common.hlsl
@@ -3,12 +3,39 @@
 
 struct HitInfo
 {
-	float4 ColorAndDistance;
+	float4 Color;
 };
 
-struct Attributes
+//test
+struct SceneBuffer
 {
-	float2 Barycentric;
+	matrix ViewProjection;
+	float4 CameraPosition;
+	float4 LightPosition;
+	float4 LightAmbient;
+	float4 LightDiffuse;
 };
+
+struct CubeVertex
+{
+	float4 CubeColor;
+};
+
+struct ObjectVertex
+{
+    float3 Vertex;
+    //float4 Color;
+	float3 Normal;
+};
+
+cbuffer cbCamera : register(b0, space0)
+{
+    float4x4 View;
+    float4x4 Projection;
+    float4x4 InversedView;
+    float4x4 InversedProjection;
+}
+
+ConstantBuffer<SceneBuffer> SceneData : register(b1, space0);
 
 #endif //COMMON_HLSL
--- DXR Demo/Assets/Raytracing/RayGen.hlsl
+++ DXR Demo/Assets/Raytracing/RayGen.hlsl
@@ -3,9 +3,42 @@
 
 #include "Common.hlsl"
 
-RWTexture2D<float4> gOutput : register(u0);
-RaytracingAccelerationStructure SceneBVH : register(t0);
+RWTexture2D<float4> RaytraceScene : register(u0);
+RaytracingAccelerationStructure SceneTopLevel : register(t0, space0);
 
+[shader("raygeneration")]
+void RayGen()
+{
+    HitInfo payload = { float4(.0f, .0f, .0f, .0f) };
+
+    uint2 launchIndex = DispatchRaysIndex().xy;
+    float2 dims = float2(DispatchRaysDimensions().xy);
+    float2 d = ((launchIndex.xy + 0.5f) / dims.xy) * 2.f - 1.f;
+    
+    float2 xy = launchIndex + 0.5f; // center in the middle of the pixel.
+    float2 screenPos = xy / DispatchRaysDimensions().xy * 2.0 - 1.0;
+
+    // Y inversion
+    screenPos.y = -screenPos.y;
+    float aspectRatio = dims.x / dims.y;
+    float4x4 viewProj = SceneData.ViewProjection;
+    float4 world = mul(float4(screenPos, 0, 1), viewProj);
+    world.xyz /= world.w;
+    float3 origin = SceneData.CameraPosition.xyz;
+    float3 direction = normalize(world.xyz - origin);
+   
+    RayDesc ray;
+    ray.Origin = origin;
+    ray.Direction = direction;
+    // TMin -> zNear, TMax -> zFar
+    ray.TMin = 0.01f;
+    ray.TMax = 100000.0f;
+    TraceRay(SceneTopLevel, RAY_FLAG_CULL_BACK_FACING_TRIANGLES, ~0, 0, 1, 0, ray, payload);
+    
+    RaytraceScene[launchIndex] = payload.Color;
+}
+
+/*
 [shader("raygeneration")]
 void RayGen()
 {
@@ -23,9 +56,10 @@ void RayGen()
     ray.TMin = 0.0f;
     ray.TMax = 100000.0f;
     
+    
     TraceRay(SceneBVH, RAY_FLAG_NONE, 0xFF, 0, 0, 0, ray, payload);
     
     gOutput[launchIndex] = float4(payload.ColorAndDistance.rgb, 1.f);
 }
-
+*/
 #endif //RAYGEN_HLSL
