f8f4439cbf1dc44430be7c047bea21005eeceaa1
LowerDeckBoy <pa.krawiec@onet.pl>
2023-09-05 19:07:57 +0200

    Changed helper functions into macros

--- DXR Demo/Source/Utilities/Utilities.hpp
+++ DXR Demo/Source/Utilities/Utilities.hpp
@@ -7,18 +7,36 @@
 #include <cassert>
 
 // For ComPtr
+#ifndef SAFE_RELEASE
 #define SAFE_RELEASE(x) { if (x) { x.Reset(); x = nullptr; } }
+#endif // SAFE_RELEASE
+
 // For non ComPtr
+#ifndef SAFE_DELETE
 #define SAFE_DELETE(x) { if (x) { x->Release(); x = nullptr; } }
+#endif // SAFE_DELETE
 
-inline void ThrowIfFailed(HRESULT hResult)
-{
-	if (FAILED(hResult))
-		throw std::runtime_error("");
-}
+// Helper macro to determinate where exactly HRESULT error occurs
+#ifndef TraceError
+#define TraceError(DebugMessage)											\
+	{																		\
+		std::string file{ "File: " + std::string(__FILE__) };				\
+		std::string func{ "\nFunction: " + std::string(__func__) };			\
+		std::string line{ "\nLine: " + std::to_string(__LINE__) + '\n' };	\
+		std::string message{ file + func + line + DebugMessage };			\
+		::OutputDebugStringA(message.c_str());								\
+		::MessageBoxA(nullptr, message.data(), "Error", MB_OK);				\
+	}																	
+#endif
 
-inline void ThrowIfFailed(HRESULT hResult, const std::string_view& Message)
-{
-	if (FAILED(hResult))
-		throw std::runtime_error(Message.data());
-}
+#ifndef ThrowIfFailed
+#define ThrowIfFailed(hResult, ...) \
+	{\
+		if (FAILED(hResult))\
+		{\
+			std::string msg{ std::string(__VA_ARGS__) };			\
+			TraceError(msg);										\
+			throw std::runtime_error("HRESULT exception thrown.");	\
+		}\
+	}
+#endif
